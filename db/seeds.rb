f5722495b7e43c0d17873c48472230757451d2f4
jareqpl <jareq02@gmail.com>
2014-03-03 12:18:13 +0100

    Gotowe zadania domowe. Seed, walidacja i utworzone modele.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+10.times do |i|
+  Group.create(name: "Grupa #{i}")
+  Event.create(
+      name: "Event #{i}",
+      start: Date.new(2000+i, 1+i, 10+i),
+      desc: "Opis #{i}",
+      invFrom: Date.new(2000+i, 1+i, 10+i),
+      invTo: Date.new(2000+i, 1+i, 12+i),
+      limitFrom: 2*i,
+      limitTo: 4*i,
+      answerTime: i,
+      showPpls: true
+  )
+  Friend.create(name: "Imie #{i}", email: "ktos#{i}@gmail.com", phone: 100000000+(i*rand(100000000)))
+end
